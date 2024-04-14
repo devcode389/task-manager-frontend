3f3e7995d68c92153ebda46be8506d5b90cb21f4
devcode389 <devcode389@gmail.com>
2024-04-14 23:55:52 +0530

    initial commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,27 @@
+const TaskForm = ({
+  createTask,
+  name,
+  handleInputChange,
+  isEditing,
+  updateTask,
+}) => {
+  return (
+    <div>
+      <form
+        className="task-form"
+        onSubmit={isEditing ? updateTask : createTask}
+      >
+        <input
+          type="text"
+          placeholder="Add a Task"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <button type="submit">{isEditing ? "Edit" : "Add"}</button>
+      </form>
+    </div>
+  );
+};
+
+export default TaskForm;
